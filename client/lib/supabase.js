3a1f26f9f69ac8fd60122fde0dc9b997b1c8e4f9
marshmel0l <67599352+marshmel0l@users.noreply.github.com>
2025-12-23 16:38:18 +0200

    Update supabase.js

--- client/lib/supabase.js
+++ client/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.azaihuzkzxrreimlmcll.supabase.co,
-  import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6YWlodXprenhycmVpbWxtY2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0OTM3OTksImV4cCI6MjA4MjA2OTc5OX0.w7zxMOKHuBrq4YownRrR_1UZNVq97vxAfZgRka_Qa8o
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
-
