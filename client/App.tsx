d4068db64a62ee044d74d0f7a0a964f7cdf2ad5a
marshmel0l <67599352+marshmel0l@users.noreply.github.com>
2025-12-24 14:51:44 +0200

    Update App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,24 @@
 import "./global.css";
 
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import RequireAdmin from "@/components/RequireAdmin";
 import AdminLayout from "@/pages/admin/AdminLayout";
+
 import { Toaster } from "@/components/ui/toaster";
-import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { CartProvider } from "@/lib/CartContext";
+
 import Index from "./pages/Index";
 import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
-import { CartProvider } from "@/lib/CartContext";
 import Login from "@/pages/Login";
-import Account from "@/pages/Account"; // we’ll add next
+
 import Account from "@/pages/Account";
 import AccountProducts from "@/pages/account/Products";
 import AccountOrders from "@/pages/account/Orders";
@@ -24,36 +28,43 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <CartProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route
-  path="/admin"
-  element={
-    <RequireAdmin>
-  }
-/>
-              <Route path="/account" element={<Account />}>
-  <Route index element={<AccountProducts />} />
-  <Route path="products" element={<AccountProducts />} />
-  <Route path="orders" element={<AccountOrders />} />
-  <Route path="reviews" element={<AccountReviews />} />
-              <Route path="/login" element={<Login />} />
-<Route path="/account" element={<Account />} />
-              <Route path="/" element={<Index />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<Cart />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </CartProvider>
-      </TooltipProvider>
-    </AdminLayout>
-     </RequireAdmin>
+    <TooltipProvider>
+      <CartProvider>
+        <Toaster />
+        <Sonner />
+
+        <BrowserRouter>
+          <Routes>
+            {/* ADMIN ROUTES */}
+            <Route
+              path="/admin/*"
+              element={
+                <RequireAdmin>
+                  <AdminLayout />
+                </RequireAdmin>
+              }
+            />
+
+            {/* ACCOUNT ROUTES */}
+            <Route path="/account" element={<Account />}>
+              <Route index element={<AccountProducts />} />
+              <Route path="products" element={<AccountProducts />} />
+              <Route path="orders" element={<AccountOrders />} />
+              <Route path="reviews" element={<AccountReviews />} />
+            </Route>
+
+            {/* PUBLIC ROUTES */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Index />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+
+            {/* CATCH-ALL */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
